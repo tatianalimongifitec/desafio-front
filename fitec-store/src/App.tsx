7583c1eaebfd6be4a70325bd575c4ac55d810d16
tatianalimongifitec <tatianalchaves@fitec.org.br>
2024-01-17 17:13:47 -0300

    Tentativas frustradas de corrigir a pagina Review pioraram o resultado

--- fitec-store/src/App.tsx
+++ fitec-store/src/App.tsx
@@ -3,7 +3,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
-import Products from './pages/Products'; 
+import Products from './pages/Products';
 import Checkout from './pages/Checkout';
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/products" element={<Products />} /> 
-        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/checkout/*" element={<Checkout />} />
       </Routes>
     </Router>
   );
